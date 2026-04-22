6f4fddb21965c42fe4bdeccced96e15c99ad8993
Freddy Arévalo <freddyarevalo2006@gmail.com>
2026-04-22 19:53:01 +0200

    Migración de código HTML y CSS

--- proyecto_slow_coffee/src/app/app.component.ts
+++ proyecto_slow_coffee/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './layout/navbar/navbar.component';
+import {HeroComponent} from './layout/hero/hero.component';
+import {FooterComponent} from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
